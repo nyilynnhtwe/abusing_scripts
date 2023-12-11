5d1ec44df2e27010a4a2b98842f52fbe2f5328e1
nyilynnhtwe <nyilynnhtwe@gmail.com>
2023-12-12 02:46:14 +0630

    added: api for deploying on vercel

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require("express")();
+const fs = require("fs");
+
+app.get("/api/heros", (req, res) => {
+  fs.readFile("data.json", "utf8", (err, file) => {
+    const data = JSON.parse(file);
+    res.send(data);
+  });
+});
+
+app.get("/api/heros/:id", (req, res) => {
+  const { id } = req.params;
+  fs.readFile('data.json', "utf8", (err, file) => {
+    // check for any errors
+    if (err) {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+          } 
+    }
+    try {
+      const data = JSON.parse(file);
+      let result;
+      for (let index = 0; index < data.length; index++) {
+        const hero = data[index];
+        if(hero.id===id)
+        {
+            result = data;
+        }
+        else
+        {
+            result = ('No valid hero');
+        }
+      }
+      res.send(result);
+    } catch (err) {
+      console.error("Error while parsing JSON data:", err);
+    }
+  });
+});
+
+module.exports = app;
