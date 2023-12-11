b90050ccc81ac7aa0d3039a9340b06976cd75666
nyilynnhtwe <nyilynnhtwe@gmail.com>
2023-12-12 02:59:45 +0630

    fixed: api for deploying on vercel

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require("express")();
 const fs = require("fs");
 
 app.get("/api/heros", (req, res) => {
+  res.send("Dota 2 heros api by Florject.");
+});
+
+app.get("/api/heros", (req, res) => {
   fs.readFile("data.json", "utf8", (err, file) => {
     const data = JSON.parse(file);
     res.send(data);
@@ -10,25 +14,22 @@ app.get("/api/heros", (req, res) => {
 
 app.get("/api/heros/:id", (req, res) => {
   const { id } = req.params;
-  fs.readFile('data.json', "utf8", (err, file) => {
+  fs.readFile("data.json", "utf8", (err, file) => {
     // check for any errors
     if (err) {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-          } 
+      if (err) {
+        res.status(500).send("Internal Server Error");
+      }
     }
     try {
       const data = JSON.parse(file);
       let result;
       for (let index = 0; index < data.length; index++) {
         const hero = data[index];
-        if(hero.id===id)
-        {
-            result = data;
-        }
-        else
-        {
-            result = ('No valid hero');
+        if (hero.id === id) {
+          result = data;
+        } else {
+          result = "No valid hero";
         }
       }
       res.send(result);
