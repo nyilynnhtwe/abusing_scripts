b8aa03adc6ceaa56392229fcf5a9ede2cc349f37
Htet Aung Khant <htetaung23.ha@gmail.com>
2023-12-13 17:20:25 +0630

    fix

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const fs = require("fs");
 const path = require("path");
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-const options = require("./doc");
+// const options = require("./doc");
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Dota2 Heros API",
+      version: "0.1.0",
+      description: "Dota 2 heros api by Florject",
+    },
+    servers: [
+      {
+        url: "https://abusing-scripts.vercel.app",
+        // url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["*.js"],
+};
 
 const specs = swaggerJsdoc(options);
 
